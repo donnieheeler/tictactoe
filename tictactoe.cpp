2a13137cecb2f0911e36b44487e022938d46c589
Donnie Heeler <bbryanruizz@gmail.com>
2022-05-26 16:07:02 -0700

    Rewriting tictactoe.cpp
    Already looks better in my opinion. Just going to keep working on this until it looks and acts decent.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,130 +3,123 @@
 
 using namespace std;
 
-int main() {
-	string tick[9] = {"1","2","3","4","5","6","7","8","9"}; //ttt array
-	int x; //declare user input pocket
-	bool win = false;
-	int y;
-	while(win == false){
+// Definitions
 
-cout << string(100, '\n');
-cout << tick[0] << " | " << tick[1] << " | " << tick[2] << "\n";
-cout << "_________\n";
-cout << tick[3] << " | " << tick[4] << " | " << tick[5] << "\n";
-cout << "_________\n";
-cout << tick[6] << " | " << tick[7] << " | " << tick[8] << "\n";
+string t3Table[9] = {"1","2","3","4","5","6","7","8","9"}; //Tic Tac Toe Table
 
-	cout << "choose 1-9:";
-	cin >> x; //user input
-	x = x - 1;
-		if(tick[x] != "O"){
-		if(tick[x] != "X"){
-		tick[x] = "X";
-		}else{
-		cout << "not valid \n";
-			}
-						if(tick[0]==tick[1] && tick[1]==tick[2]){
-						win = true;
-						tick[0] = tick[1] = tick [2] = "W";		
-						}
-						if(tick[3]==tick[4] && tick[4]==tick[5]){
-						win = true;
-						tick[3] = tick[4] = tick [5] = "W";				
-						}
-						if(tick[6]==tick[7] && tick[7]==tick[8]){
-						win = true;	
-						tick[6] = tick[7] = tick [8] = "W";			
-						}	
-						if(tick[0]==tick[3] && tick[3]==tick[6]){
-						win = true;		
-						tick[0] = tick[3] = tick [6] = "W";		
-						}
-						if(tick[1]==tick[4] && tick[4]==tick[7]){
-						win = true;	
-						tick[1] = tick[4] = tick [7] = "W";	
-						}
-						if(tick[2]==tick[5] && tick[5]==tick[8]){
-						win = true;
-						tick[2] = tick[5] = tick [8] = "W";			
-						}
-						if(tick[0]==tick[4] && tick[4]==tick[8]){
-						win = true;	
-						tick[0] = tick[4] = tick [8] = "W";		
-						}	
-						if(tick[6]==tick[4] && tick[4]==tick[2]){
-						win = true;	
-						tick[6] = tick[4] = tick [2] = "W";		
-						}
+bool winState = false; // Declare win state.
+
+int playerChoice; //Declare user input.
+
+int computerChoice; //Declare computer choice.
+
+bool retry;
+
+// Display Function: Displays T3 Table
+
+void display() {
+
+	cout << t3Table[0] << " | " << t3Table[1] << " | " << t3Table[2] << "\n";
+	cout << "_________\n";
+	cout << t3Table[3] << " | " << t3Table[4] << " | " << t3Table[5] << "\n";
+	cout << "_________\n";
+	cout << t3Table[6] << " | " << t3Table[7] << " | " << t3Table[8] << "\n";
+}
+
+void choice() {
+
+	cout << "Choose 1 - 9, or press 0" << "\n";
+
+	cin >> playerChoice;
+
+	if(playerChoice == 0){
+		do{
+			retry = false;
+			std::random_device rd;
+			std::mt19937 gen (rd());
+			uniform_int_distribution<> distr(1,9);
+			int computerChoice = distr(gen);
+			computerChoice = computerChoice - 1;
+
+			if(t3Table[computerChoice] != "O"){
+			if(t3Table[computerChoice] != "X"){
+			t3Table[computerChoice] = "O";
+			retry = true;
+			    }
+			    }
+	        }
+			while(retry == false);
+	       computerChoice = computerChoice - 1;
+    }else{
+			playerChoice = playerChoice - 1; //Preps user input to work with  t3Table
 			
-			bool retry;
-			do{
-			    retry = false;
-				std::random_device rd;
-				std::mt19937 gen(rd());
-				uniform_int_distribution<> distr(1,9);
-				int y = distr(gen);
-				//cout << y;
-				y=y-1;
+			if(t3Table[playerChoice] != "O"){
+			if(t3Table[playerChoice] != "X"){
+			t3Table[playerChoice] = "X";
+				}else{
+				cout << "not valid \n";
+				}
 				
-					if(tick[y] != "O"){
-					if(tick[y] != "X"){
-					tick[y] = "O";
-					retry = true;
-					}}else{
-                    //nothing
-					}
-			}
-					while(retry == false);
-					y=y-1;
-					
-						if(tick[0]==tick[1] && tick[1]==tick[2]){
-						win = true;
-						tick[0] = tick[1] = tick [2] = "W";		
-						}
-						if(tick[3]==tick[4] && tick[4]==tick[5]){
-						win = true;
-						tick[3] = tick[4] = tick [5] = "W";				
-						}
-						if(tick[6]==tick[7] && tick[7]==tick[8]){
-						win = true;	
-						tick[6] = tick[7] = tick [8] = "W";			
-						}	
-						if(tick[0]==tick[3] && tick[3]==tick[6]){
-						win = true;		
-						tick[0] = tick[3] = tick [6] = "W";		
-						}
-						if(tick[1]==tick[4] && tick[4]==tick[7]){
-						win = true;	
-						tick[1] = tick[4] = tick [7] = "W";	
-						}
-						if(tick[2]==tick[5] && tick[5]==tick[8]){
-						win = true;
-						tick[2] = tick[5] = tick [8] = "W";			
-						}
-						if(tick[0]==tick[4] && tick[4]==tick[8]){
-						win = true;	
-						tick[0] = tick[4] = tick [8] = "W";		
-						}	
-						if(tick[6]==tick[4] && tick[4]==tick[2]){
-						win = true;	
-						tick[6] = tick[4] = tick [2] = "W";		
-						}
-			}
-		cout << string(100, '\n');
-		}
-	cout << string(100, '\n');
-	cout << "Game Over\n";
-	cout << tick[0] << " | " << tick[1] << " | " << tick[2] << "\n";
-	cout << "_________\n";
-	cout << tick[3] << " | " << tick[4] << " | " << tick[5] << "\n";
-	cout << "_________\n";
-	cout << tick[6] << " | " << tick[7] << " | " << tick[8] << "\n";
+            }
+    	}
+}
 
-	/*for (int i = 0; i < 4; i++) 
-	{
-  	cout << tick[i] << "\n";
-	}*/
 
-  return 0;
+
+/*
+//Needs major overhaul. Feels like there is a better way to write this.
+//Check to see if there is a win
+
+void winCheck() {
+
+if(tick[0]==tick[1] && tick[1]==tick[2]){
+win = true;
+tick[0] = tick[1] = tick [2] = "W";		
+}
+
+if(tick[3]==tick[4] && tick[4]==tick[5]){
+win = true;
+tick[3] = tick[4] = tick [5] = "W";				
+}
+
+if(tick[6]==tick[7] && tick[7]==tick[8]){
+win = true;	
+tick[6] = tick[7] = tick [8] = "W";			
+}	
+
+if(tick[0]==tick[3] && tick[3]==tick[6]){
+win = true;		
+tick[0] = tick[3] = tick [6] = "W";		
+}
+
+if(tick[1]==tick[4] && tick[4]==tick[7]){
+win = true;	
+tick[1] = tick[4] = tick [7] = "W";	
+}
+
+if(tick[2]==tick[5] && tick[5]==tick[8]){
+win = true;
+tick[2] = tick[5] = tick [8] = "W";			
+}
+
+if(tick[0]==tick[4] && tick[4]==tick[8]){
+win = true;	
+tick[0] = tick[4] = tick [8] = "W";		
+}	
+
+if(tick[6]==tick[4] && tick[4]==tick[2]){
+win = true;	
+tick[6] = tick[4] = tick [2] = "W";
+		
+}
+*/
+
+int main() {
+
+display();
+
+choice();
+
+return 0;
+
 }
