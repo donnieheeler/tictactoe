01dcb0a54fa355e1b743f89690bcdf1355b45633
Donnie Heeler <bbryanruizz@gmail.com>
2022-06-07 15:08:24 -0700

    Update tictactoe.cpp
    Way cleaner. Really focusing on breaking everything down and making it all work together. Call it Polymorphic Object Oriented Programming, or P.O.O.P for short.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-// Definitions
+//Variables
 
 string t3Table[9] = {"1","2","3","4","5","6","7","8","9"}; //Tic Tac Toe Table
 
@@ -12,6 +12,8 @@ bool winState = false; // Declare win state.
 int playerChoice; //Declare user input.
 
 int computerChoice; //Declare computer choice.
+
+string xoro = "X";
 
 bool retry;
 
@@ -26,100 +28,40 @@ void display() {
 	cout << t3Table[6] << " | " << t3Table[7] << " | " << t3Table[8] << "\n";
 }
 
+
+// Chooses a random number 0 - 8
+
 void choice() {
+	std::random_device rd;
+	std::mt19937 gen (rd());
+	uniform_int_distribution<> distr(0,8);
+	int computerChoice = distr(gen);
+	validate();
+}
 
-	cout << "Choose 1 - 9, or press 0" << "\n";
+// Switches from X to O or, O to X
+void xoroSwitch() {
+	if(xoro == "X"){
+	xoro = "O";
+	}else{
+	xoro = "X";
+	}		
+}
 
-	cin >> playerChoice;
-
-	if(playerChoice == 0){
-		do{
-			retry = false;
-			std::random_device rd;
-			std::mt19937 gen (rd());
-			uniform_int_distribution<> distr(1,9);
-			int computerChoice = distr(gen);
-			computerChoice = computerChoice - 1;
-
-			if(t3Table[computerChoice] != "O"){
-			if(t3Table[computerChoice] != "X"){
-			t3Table[computerChoice] = "O";
-			retry = true;
-			    }
-			    }
-	        }
-			while(retry == false);
-	       computerChoice = computerChoice - 1;
-    }else{
-			playerChoice = playerChoice - 1; //Preps user input to work with  t3Table
-			
-			if(t3Table[playerChoice] != "O"){
-			if(t3Table[playerChoice] != "X"){
-			t3Table[playerChoice] = "X";
-				}else{
-				cout << "not valid \n";
-				}
-				
-            }
-    	}
+// Sets the validated move as X or O then preps for the next move by calling xoroSwitch
+void set() {
+	t3Table[computerChoice] = xoro;
+	xoroSwitch();
 }
 
 
-
-/*
-//Needs major overhaul. Feels like there is a better way to write this.
-//Check to see if there is a win
-
-void winCheck() {
-
-if(tick[0]==tick[1] && tick[1]==tick[2]){
-win = true;
-tick[0] = tick[1] = tick [2] = "W";		
-}
-
-if(tick[3]==tick[4] && tick[4]==tick[5]){
-win = true;
-tick[3] = tick[4] = tick [5] = "W";				
-}
-
-if(tick[6]==tick[7] && tick[7]==tick[8]){
-win = true;	
-tick[6] = tick[7] = tick [8] = "W";			
-}	
-
-if(tick[0]==tick[3] && tick[3]==tick[6]){
-win = true;		
-tick[0] = tick[3] = tick [6] = "W";		
-}
-
-if(tick[1]==tick[4] && tick[4]==tick[7]){
-win = true;	
-tick[1] = tick[4] = tick [7] = "W";	
-}
-
-if(tick[2]==tick[5] && tick[5]==tick[8]){
-win = true;
-tick[2] = tick[5] = tick [8] = "W";			
-}
-
-if(tick[0]==tick[4] && tick[4]==tick[8]){
-win = true;	
-tick[0] = tick[4] = tick [8] = "W";		
-}	
-
-if(tick[6]==tick[4] && tick[4]==tick[2]){
-win = true;	
-tick[6] = tick[4] = tick [2] = "W";
-		
-}
-*/
-
-int main() {
-
-display();
-
-choice();
-
-return 0;
-
+// Validates the computers choice
+void validate() {
+	if(t3Table[computerChoice] != "O"){
+	if(t3Table[computerChoice] != "X"){
+	set();
+	}else{
+	choice();
+	}
+	}
 }
